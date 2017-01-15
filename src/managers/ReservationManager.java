84eace3ef7f654c90c85f631a8c3130183e2776d
pablocabezali <paucabla@opendeusto.es>
2017-01-15 18:10:13 +0100

    PARTE Cabezali HECHA

--- src/managers/ReservationManager.java
+++ src/managers/ReservationManager.java
@@ -1,21 +1,29 @@
 package managers;
 
+import db.EasyBookingDaoImplement;
 import entities.Flight;
 import entities.Reservation;
 import entities.User;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pablocabezali on 15/1/17.
  */
 public class ReservationManager {
+    EasyBookingDaoImplement a = new EasyBookingDaoImplement();
 
-    public Flight[] createFlight(){
-        Flight [] temp;
-        return temp;
+    public boolean createReservation(User u, Flight f) {
+        Reservation r = new Reservation();
+        r.addFlight(f);
+        u.addReservation(r);
+
+        a.storeReservation(r);
     }
 
-    public Reservation[] createReservation(User u){
-        Reservation [] temp;
-        return temp;
+    public List<Reservation> getReservations(User u) {
+        List<Reservation> reserveList = new ArrayList<>();
+        reserveList = u.getReservation();
+        return reserveList;
     }
 }
